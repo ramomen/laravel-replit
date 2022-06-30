a44a5adad44600e20f661f3de399b8c82cb843b3
Ramazan Değirmenci <45834008+ramomen@users.noreply.github.com>
2022-06-30 15:27:04 +0300

    dd

--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -1,3 +1,4 @@
+
 <?php
 
 namespace App\Http\Controllers;
@@ -6,6 +7,8 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+
 
 class Controller extends BaseController
 {
